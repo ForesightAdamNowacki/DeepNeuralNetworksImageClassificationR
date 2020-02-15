2d919f5f180faa3bbb20f12fe2d6955078080ae5
ForesightAdamNowacki <adam.nowacki95@gmail.com>
2020-02-15 01:32:58 +0100

    Commit 3

--- Binary_Model_Evaluation.R
+++ Binary_Model_Evaluation.R
@@ -143,30 +143,6 @@ Binary_Classifier_Verification <- function(actual,
                              "Assessment_of_Classifier_Effectiveness" = result_3))
 } 
 # ------------------------------------------------------------------------------
-# Data: Johns Hopkins University Ionosphere database
-# Description: Predict high-energy structures in the atmosphere from antenna data.
-# Type: Classification
-# Dimensions: 351 instances, 35 attributes
-# Inputs: Numeric
-# Output: Categorical, 2 class labels
-# UCI Machine Learning Repository:
-utils::browseURL(url = "https://archive.ics.uci.edu/ml/datasets/Ionosphere")
-base::library(mlbench)
-base::library(randomForest)
-utils::data(Ionosphere)
-data <- Ionosphere
-data %>%
-  tibble::as_tibble(.) %>%
-  dplyr::mutate(V1 = NULL,
-                V2 = NULL,
-                Class = base::factor(base::ifelse(Class == "good", 1, 0))) -> data; data
-
-model <- randomForest::randomForest(Class ~ ., data = data, ntree = 100)
-predicted <- stats::predict(model, data, "prob")[,2]
-actual <- base::as.numeric(data$Class) - 1
-
-Binary_Classifier_Verification(actual = actual, predicted = predicted, cutoff = 0.25)
-# ------------------------------------------------------------------------------
 # CUT-OFF OPTIMIZATION
 # Function to optimize the cut-off level in relation to many evaluation metrics.
 Binary_Classifier_Cutoff_Optimization <- function(actual,
@@ -325,28 +301,4 @@ Binary_Classifier_Cutoff_Optimization <- function(actual,
                                                           nrow = 4, ncol = 4))
   knitr::kable(df) %>% base::print(.)
 }
-# ------------------------------------------------------------------------------
-# Data: Johns Hopkins University Ionosphere database
-# Description: Predict high-energy structures in the atmosphere from antenna data.
-# Type: Classification
-# Dimensions: 351 instances, 35 attributes
-# Inputs: Numeric
-# Output: Categorical, 2 class labels
-# UCI Machine Learning Repository:
-utils::browseURL(url = "https://archive.ics.uci.edu/ml/datasets/Ionosphere")
-base::library(mlbench)
-base::library(randomForest)
-utils::data(Ionosphere)
-data <- Ionosphere
-data %>%
-  tibble::as_tibble(.) %>%
-  dplyr::mutate(V1 = NULL,
-                V2 = NULL,
-                Class = base::factor(base::ifelse(Class == "good", 1, 0))) -> data; data
-
-model <- randomForest::randomForest(Class ~ ., data = data, ntree = 100)
-predicted <- stats::predict(model, data, type = "prob")[,2]
-actual <- base::as.numeric(data$Class) - 1
-
-Binary_Classifier_Cutoff_Optimization(actual = actual, predicted = predicted, cuts = 50)
 # ------------------------------------------------------------------------------
